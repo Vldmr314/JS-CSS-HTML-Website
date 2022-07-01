faed0731df6f924430e6537b869decb3c7228055
Vladimir <vladimir314v@gmail.com>
2022-07-01 15:23:56 +0300

    Initial

---
+++ src/lib/main_page/Contact.svelte
@@ -0,0 +1,75 @@
+<div class="contact" id="contact">
+    <h1>Contact Me</h1>
+
+    <div class="contact-links">
+        <a href="https://github.com/Vldmr314" target="_blank"><img src="./img/socials/git_icon.png"
+                                                                   alt="github"/></a>
+        <a href="https://vk.com/vladimir31415" target="_blank"><img src="./img/socials/vk_icon.png" alt="vk"/></a>
+        <a href="https://www.instagram.com/_.mr_volodya._" target="_blank"><img src="./img/socials/inst_icon.png"
+                                                                                alt="instagram"/></a>
+        <a href="https://t.me/Vldmr11" target="_blank"><img src="./img/socials/tg_icon.png" alt="telegram"/></a>
+    </div>
+
+    <footer>
+        <p>© Rights reserved 2022</p>
+    </footer>
+</div>
+
+<style>
+    .contact {
+        margin: 0;
+        position: absolute;
+        top: 300vh;
+        height: 100vh;
+        width: 100vw;
+        background: url("/img/backgrounds/contact_back.jpeg") no-repeat;
+        background-size: cover;
+        color: white;
+    }
+
+    h1 {
+        font-family: "Xirod", sans-serif;
+        letter-spacing: 3px;
+        font-size: 46px;
+        display: flex;
+        justify-content: center;
+        margin-top: 5vh;
+    }
+
+    .contact-links {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+        align-items: center;
+        margin: 0 auto;
+        max-width: 640px;
+        padding: 16px 8px 0 8px;
+    }
+
+    a {
+        margin-top: 9%;
+    }
+
+    img {
+        transition: 0.2s;
+        width: 48px;
+        will-change: filter;
+    }
+
+    img:hover {
+        transform: scale(1.2);
+    }
+
+    footer {
+        display: flex;
+        text-align: center;
+    }
+
+    p {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 1vh;
+        z-index: 10;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import Intro from "../lib/main_page/Intro.svelte";
+    import About from "../lib/main_page/About.svelte";
+    import Portfolio from "../lib/main_page/Portfolio.svelte";
+    import Contact from "../lib/main_page/Contact.svelte";
+</script>
+
+<svelte:head>
+    <title>Vladimir Ryabenko</title>
+
+    <meta name="author" content="Vladimir Ryabenko"/>
+    <meta name="description" property="og:title" content="Personal Website of Vladimir Ryabenko"/>
+    <meta name="image" property="og:image" content="./img/other/my_photo.jpg"/>
+    <meta name="robots" content="all"/>
+    <meta name="google" content="notranslate"/>
+</svelte:head>
+
+<main>
+    <Intro/>
+    <About/>
+    <Portfolio/>
+    <Contact/>
+</main>
+
+<style>
+    @font-face {
+        font-family: "Xirod";
+        src: url("/fonts/xirod.woff2") format("woff2"),
+        url("/fonts/xirod.woff") format("woff");
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    :global(body) {
+        padding: 0;
+        margin: 0;
+        overflow-x: hidden;
+        text-shadow: 0 0 7px white,
+        0 0 42px aqua,
+        0 0 82px aqua;
+    }
+</style>
